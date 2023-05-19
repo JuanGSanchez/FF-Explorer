f41b11fceea02357cdf77988ea393d9fcf9fa827
JuanGSanchez <125212909+JuanGSanchez@users.noreply.github.com>
2023-05-19 20:45:41 +0200

    Solve minor manual issue in combobox

--- FF_UI.py
+++ FF_UI.py
@@ -56,7 +56,7 @@ class FFE_UI(Tk):
         self.style_opts = {'bg' : '#bfbfbf', 'fg' : 'black', 'font' : 'Verdana 12', 'activebackground' : '#bfbfbf', 'activeforeground' : 'green'}
         self.font_title = {'bg' : '#999999', 'fg' : 'blue', 'font' : 'Arial 12 bold'}
         self.font_entry = {'bg' : 'white', 'fg' : 'black', 'font' : 'Verdana 11'}
-        self.font_text = {'bg' : 'darkblue', 'font' : "Arial 11", 'fg' : 'white'}
+        self.font_man = {'bg' : 'darkblue', 'font' : "Arial 11", 'fg' : 'white'}
 
 # UI variables
         self.source = StringVar(value = '*Select path here*')   # Root path variable
@@ -65,7 +65,7 @@ class FFE_UI(Tk):
         self.dict_options = {'*Select action*': -1, 'Save list': 1, 'Remove list': 2, 'Compress list': 3}   # List with options to handle the directory obtained
 
 # UI layout
-        '''Source path selection, from which all nested files or folders will be listed'''
+        ''' Source path selection, from which all nested files or folders will be listed '''
         lab_path = Label(self, text = "Root path", justify = CENTER, bd = 2, width = 18, **self.font_title)
         lab_path.grid(row = 0, column = 0, padx = 10, pady = 7, ipadx = 10, ipady = 5)
         self.ent_path = Entry(self, textvariable = self.source, justify = LEFT, bd = 5, relief = SUNKEN, state = "readonly", cursor = "hand2", width = 18, **self.font_entry)
@@ -73,15 +73,15 @@ class FFE_UI(Tk):
         self.ent_path.bind("<1>", self.source_selection)
         self.ent_path.bind("<MouseWheel>", lambda event: self.ent_path.xview_scroll(int(event.delta/40), 'units'))
 
-        '''Stablishment of a name seed, a chain of characters that must be included in the file/folder name.
-        It acts as a filter tool; by leaving it blank, all nested files/folders will be listed'''
+        ''' Stablishment of a name seed, a chain of characters that must be included in the file/folder name.
+        It acts as a filter tool; by leaving it blank, all nested files/folders will be listed '''
         lab_filter = Label(self, text = "Name seed", justify = CENTER, bd = 2, width = 18, **self.font_title)
         lab_filter.grid(row = 2, column = 0, padx = 10, pady = 7, ipadx = 10, ipady = 5)
         ent_filter = Entry(self, textvariable = self.seed, justify = LEFT, bd = 5, relief = SUNKEN, width = 18, **self.font_entry)
         ent_filter.grid(row = 3, column = 0, padx = 10, pady = 5, ipadx = 10, ipady = 5)
         ent_filter.bind("<MouseWheel>", lambda event: ent_filter.xview_scroll(int(event.delta/40), 'units'))
 
-        '''Selection block, type of files and action to be performed'''
+        ''' Selection block, type of files and action to be performed '''
         Rd_opt1 = Radiobutton(self, text = "Folders", var = self.d_type, value = 0, justify = LEFT, bd = 2, **self.style_opts)
         Rd_opt1.grid(row = 4, column = 0, padx = 15, pady = 7, ipadx = 1, ipady = 5, sticky = W)
         Rd_opt2 = Radiobutton(self, text = "Files", var = self.d_type, value = 1, justify = LEFT, bd = 2, **self.style_opts)
@@ -90,7 +90,7 @@ class FFE_UI(Tk):
         self.Cb_opt3.set(list(self.dict_options.keys())[0])
         self.Cb_opt3.grid(row = 5, column = 0, padx = 10, pady = 5, ipadx = 10, ipady = 5)
 
-        '''Run button'''
+        ''' Run button '''
         but_run = Button(self, text = "Run", bd = 3, relief = RAISED, command = self.accept, **self.style_but, width = 5)
         but_run.grid(row = 6, column = 0, padx = 10, pady = 10, ipadx = 10, ipady = 5)
 
@@ -107,13 +107,14 @@ class FFE_UI(Tk):
         fr_man.overrideredirect(True)
         fr_man.wm_attributes('-alpha', 0.8)
         fr_man.withdraw()
-        self.fr_lab = Label(fr_man, justify = LEFT, bd = 2, **self.font_text)
+        self.fr_lab = Label(fr_man, justify = LEFT, bd = 2, **self.font_man)
         self.fr_lab.grid(padx = 1, pady = 1, sticky = W)
         self.ent_path.bind("<Motion>", lambda event : self.show_manual(event, fr_man, [211, 39, 200, 45], text_man1))
         ent_filter.bind("<Motion>", lambda event : self.show_manual(event, fr_man, [211, 39, 220, 45], text_man2))
         Rd_opt1.bind("<Motion>", lambda event : self.show_manual(event, fr_man, [90, 30, 115, 30], text_man3))
         Rd_opt2.bind("<Motion>", lambda event : self.show_manual(event, fr_man, [70, 30, 100, 30], text_man4))
         self.Cb_opt3.bind("<Motion>", lambda event : self.show_manual(event, fr_man, [200, 30, 165, 45], text_man5))
+        self.Cb_opt3.bind("<<ComboboxSelected>>", lambda event : self.show_manual_action(fr_man, [70, 30, 165, 45], text_man5))
 
 # UI contextual menu
         self.menucontext = Menu(self, tearoff = 0)
@@ -130,9 +131,8 @@ class FFE_UI(Tk):
         self.mainloop()
 
 
-
 # Additional functions of the class
-    '''Source directory selection function'''
+    ''' Source directory selection function '''
     def source_selection(self, event):
         adress = filedialog.askdirectory(initialdir = "", title = "FF Explorer, root path selection")
         if adress != '':
@@ -140,7 +140,24 @@ class FFE_UI(Tk):
             self.ent_path.xview_moveto(1)
 
 
-    ''' Show widget manual'''
+    ''' Show auxiliar manual widget '''
+    def show_manual_action(self, fr, pos, text_man):
+        self.fr_lab.config(text = text_man + self.aux_man[self.dict_options[self.Cb_opt3.get()]])
+        match self.dict_options[self.Cb_opt3.get()]:
+            case -1:
+                pass
+            case 0:
+                pass
+            case 1:
+                pos[2], pos[3] = 250, 63
+            case 2:
+                pos[2], pos[3] = 185, 63
+            case 3:
+                pos[2], pos[3] = 285, 78
+        fr.geometry('{}x{}+{}+{}'.format(pos[2], pos[3], self.winfo_pointerx() + 20, self.winfo_pointery() + 20))
+
+
+    ''' Show manual widget '''
     def show_manual(self, e, fr, pos, text_man):
         if 0 < e.x < pos[0] and 0 < e.y < pos[1]:
             fr.deiconify()
@@ -164,12 +181,12 @@ class FFE_UI(Tk):
             fr.withdraw()
 
 
-    ''' Show contextual menu'''
+    ''' Show contextual menu '''
     def show_menucontext(self, e):
         self.menucontext.post(e.x_root, e.y_root)
 
 
-    '''Run function'''
+    ''' Run function '''
     def accept(self):
         if self.source.get() == '*Select path here*':   # Check if path is introduced
             messagebox.showwarning("Warning!", "Source path not added")
@@ -184,7 +201,7 @@ class FFE_UI(Tk):
                 print('  No {} was found, nothing was {}.'.format('file' if self.d_type.get() else 'folder', ending))
 
 
-    '''Exit function'''
+    ''' Exit function '''
     def exit(self):
         print('Exiting FF Explorer...')
         self.quit()
